c2a823269e989e14ba0e61467f127bccb7b8146e
KhalO <Khan.maytok@gmail.com>
2025-05-30 12:12:45 -0500

    sd

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-//const { createAdapter } = require('@socket.io/redis-adapter');
 
-// Redis clients
-//const pubClient = createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
-//const subClient = pubClient.duplicate();
+// Import CORS middleware
+const cors = require("cors");
 
-// Socket.IO server
-io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
+// Enable CORS for Express routes
+app.use(
+  cors({
+    origin: "*", // Allow requests from this origin
+    methods: ["GET", "POST"],         // Allowed HTTP methods
+    credentials: true                 // Allow cookies/credentials
+  })
+);
 
-  // Handle "sent_product" event
-  socket.on('sent_product', (data) => {
-    console.log('Order received:', data);
-    io.emit('new_order', data);
+// Socket.IO server with CORS config
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow requests from this origin
+    methods: ["GET", "POST"],        // Allowed HTTP methods
+    credentials: true                // Allow cookies/credentials
+  }
+});
+
+// Socket.IO logic
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("sent_product", (data) => {
+    console.log("Order received:", data);
+    io.emit("new_order", data); // Broadcast to all clients
   });
 
-  socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+// Basic Express route
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
 
 // Start server
